9ba1914cd483d005002cabe7abdd96579a86682c
xmly <xmly@xmlydeMacBook-Pro.local>
2021-02-25 11:38:41 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route  } from "react-router-dom";
 
 function App() {
   return (
@@ -22,4 +23,33 @@ function App() {
   );
 }
 
-export default App;
+function App1 (){
+  return (
+    <BrowserRouter basename='react'>
+      <Link to="/">首页</Link>
+      <Link to="/about">关于</Link>
+      <Route path="/" exact render={() => (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )}></Route>
+      <Route path="/about" exact render={() => <div>关于</div>}></Route>
+    </BrowserRouter>
+  )
+}
+
+// export default App;
+export default App1;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App1 from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+function render(){
+  ReactDOM.render(
+    <React.StrictMode>
+      <App1 />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+
+if(!window.__POWERED_BY_QIANKUN__){
+  console.log('非qiankun模式')
+  render()
+} else {
+  console.log('qiankun模式')
+  render()
+
+}
+
+export async function bootstrap(){
+
+}
+
+export async function mount(props) {
+  render()
+}
+
+export async function unmount(){
+  console.log('------ unmount -------->')
+  ReactDOM.unmountComponentAtNode(document.getElementById('root'))
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
